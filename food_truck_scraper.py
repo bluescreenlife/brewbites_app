45abd0985b8b12748237808b43f90ac71985af80
bluescreenlife <bluescreenlife91@gmail.com>
2023-12-04 15:34:39 -0600

    first commit

---
+++ food_truck_scraper.py
@@ -0,0 +1,148 @@
+from inspect import Traceback
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import html
+
+current_date_time = datetime.datetime.now()
+today = current_date_time.strftime("%A")
+date_str = current_date_time.strftime("%Y-%m-%d")
+
+brewery_data = [
+    {
+    "name": "bauhaus",
+    "URL": "https://www.bauhausbrewlabs.com/food",
+    "html tag": "p",
+    "class rule": "sqsrte-large preFade fadeIn"
+    },
+    {"name": "headflyer",
+     "URL": "https://www.headflyerbrewing.com/food/",
+    "html tag": "div",
+    "class rule": "font-fjalla pb-2"
+    },
+    {"name": "forgotten star",
+    "URL": "https://www.forgottenstarbrewing.com/food-drink",
+    "html tag": "h3",
+    "class rule": "agenda-item-title mt-0 mb-1 ellipsized bc-agenda-desc-color"
+    },
+    {"name": "falling knife",
+    "URL": "https://fallingknife.beer/", # no truck schedule on site
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "56",
+    "URL": "https://56brewing.com/events/",
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "indeed",
+    "URL": "https://www.indeedbrewing.com/",
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "bent brewstillery",
+     "URL": "https://www.bentbrewstillery.com/",
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "broken clock",
+     "URL": "https://www.brokenclockbrew.com/events/",
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "fair state",
+     "URL": "https://fairstate.coop/events/?fwp_event_type=food",
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "sociable ciderwerks",
+     "URL": "https://sociablecider.com/sociablefoodtruck",
+    "html tag": None,
+    "class rule": None
+    },
+    {"name": "insight",
+     "URL": "https://www.insightbrewing.com/food-trucks-events",
+    "html tag": None,
+    "class rule": None
+    }
+]
+
+# def make_soup(url):
+#     response = requests.get(url)
+#     return BeautifulSoup(response.text, "html.parser")
+
+# def get_truck(soup, html_tag, class_rule):
+#     try:
+#         return soup.find(html_tag, class_=class_rule).get_text()
+#     except:
+#         return soup.find(html_tag, class_=class_rule)
+
+# for brewery in brewery_data:
+#     if brewery["html tag"] and brewery["class rule"]:
+#         soup = make_soup(brewery["URL"])
+#         truck = get_truck(soup, brewery["html tag"], brewery["class rule"])
+#         print(f"{brewery['name'].title()}: {truck}")
+
+# ----------------- fetching rules ----------------- #
+
+def forgotten_star(): # work in progress
+    response = requests.get("https://www.forgottenstarbrewing.com/food-drink")
+    forgotten_star = response.text
+    soup = BeautifulSoup(forgotten_star, "html.parser")
+    element = soup.find('h3:contains("Deep Roots")')
+    # inner = outer.find("h3", class_="agenda-item-title mt-0 mb-1  bc-agenda-desc-color")
+    print(element) 
+
+def bauhaus(): # working
+    response = requests.get("https://www.bauhausbrewlabs.com/food")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    schedule = soup.find("div", class_="sqs-html-content")
+    truck = schedule.select(f'p:-soup-contains("{today.upper()}")')[0].get_text().split("-")[1].strip()
+    return truck.title()
+
+def fifty_six():
+    response = requests.get("https://56brewing.com/events/")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    schedule = soup.find("div", class_="tribe-events-calendar-month__day tribe-events-calendar-month__day--current")
+    truck = schedule.select_one('a:-soup-contains("Food")').get_text().split(":")[1].strip()
+    print(truck)
+
+def bent(): # no current trucks listed, check later
+    response = requests.get("https://www.bentbrewstillery.com/")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find("span", class_="wixui-rich-text__text")
+    print(element)
+
+def broken_clock(): # no current trucks listed, check back later
+    response = requests.get("https://www.brokenclockbrew.com/events/")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find()
+    print(element)
+
+def fair_state(): # no current trucks listed, check back later
+    response = requests.get("")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find()
+    print(element)
+
+def sociable_ciderwerks():
+    response = requests.get("https://sociablecider.com/sociablefoodtruck")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find("a", href="/sociablefoodtruck").get_text().strip()
+    print(element)
+
+def insight():
+    response = requests.get("https://www.insightbrewing.com/food-trucks-events")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find("div", id={date_str})
+    truck = element.find()
+    print(element)
+
+print(date_str)
